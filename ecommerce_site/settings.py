c3abd06993ec06e95e9ea60701229e28d6ca9f40
Declan <decofeco29@gmail.com>
2017-12-04 21:38:24 +0000

    password reset send to email working

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -134,7 +134,14 @@ AUTHENTICATION_BACKENDS = [
     'accounts.backends.EmailAuth'
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True       
+EMAIL_HOST = 'smtp.gmail.com'      
+EMAIL_PORT = 587     
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')   
+# SERVER_EMAIL = os.environ.get('SERVER_EMAIL')   
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')     
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
